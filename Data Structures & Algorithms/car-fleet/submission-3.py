3c8551d60cde04992b21260e2cf83caf09dff609
Anuar Afiq <anuarafiq2407@gmail.com>
2026-05-30 21:11:24 +0800

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        if len(position) == 1: return 1
+        fleets = []
+        count = 0
+        curr_fleet = -1
+        pos_speed = sorted(zip(position, speed), key=lambda x: x[0], reverse=True)
+        time = [-((-target+pos) // speed) for pos, speed in pos_speed]
+
+        for t in time:
+            if t >= curr_fleet:
+                fleets.append(t)
+                curr_fleet = max(curr_fleet, t)
+                count += 1
+
+        return count-1
