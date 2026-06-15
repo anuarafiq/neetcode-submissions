9c6a5c3ad2cce3fddf0ac858d209a8e5f90bf8bc
Anuar Afiq <anuarafiq2407@gmail.com>
2026-06-15 17:22:03 +0800

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str):
+        k = len(s1)
+        n = len(s2)
+        s1_permute = Counter(s1)
+
+        for i in range(k, n):
+            if Counter(s2[i-k:i]) == s1_permute: return True
+
+        return False
